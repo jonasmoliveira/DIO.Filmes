61c5e171d05c7719a7e5ea1bd9f2a873530bbff0
Jonas M Oliveira <jmello.oliveira@yahoo.com.br>
2022-01-31 22:02:05 -0300

    DIO Projeto 2: App de cadastro de filmes

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Filmes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
